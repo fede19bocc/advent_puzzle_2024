08259660994d1049a9df46989ff4230e1ef0d46e
Federico Boccazzi <fedeboccazzi@gmail.com>
2024-12-03 12:30:47 -0300

    Puzzle resuelto, fallaba por como estaba generada la regex

--- dia 3/puzzle-3.py
+++ dia 3/puzzle-3.py
@@ -32,14 +32,29 @@ def multiplicarYsumar(multiplicaciones, formato):
            valores.append(int(aux[0])*int(aux[1]))
     return valores
 
+def memoriaDañada(datos, regex):
+    dañado = []
+    for memoria in datos:
+        aux = re.split(regex, memoria)
+        dañado.append(aux)
+    return dañado
+
 #%%
 
 datos = leer_txt('input.txt')
-regex = '(?i)(mul\([0-9]*\,[0-9]*\))+'
-multiplicaciones = interpretar(datos, regex)
-
+# regex1 = '(?i)(mul\([0-9]*\,[0-9]*\))+' # agrupaba dos mul()mul() juntos
+regex2 = '(?i)mul\([0-9]*\,[0-9]*\)' #separa individualmente cada mul()
+# multiplicaciones1 = interpretar(datos, regex1)
+multiplicaciones2 = interpretar(datos, regex2)
+# for i in range(len(multiplicaciones1)):
+#     print(i)
+#     for mem in multiplicaciones2[i]:
+#         if mem not in multiplicaciones1[i]:
+#             print(mem)
 formato = '[0-9]+'
 
-valores = multiplicarYsumar(multiplicaciones, formato)
+valores = multiplicarYsumar(multiplicaciones2, formato)
+
+dañado = memoriaDañada(datos, regex2)
 
 print(sum(valores))
