7c23032279e7dc38439147c4f734e3f7ad0c28d3
Fede <fedeboccazzi@gmail.com>
2024-12-08 11:12:06 -0300

    subo solucion parcial a la parte 2, funciona con los datos para test. Pero no es correcta con el input del problema

--- dia 5/puzzle-5.py
+++ dia 5/puzzle-5.py
@@ -15,6 +15,10 @@ def leer_txt(archivo):
     return lista
 
 def procesar_txt(txt):
+    '''
+    devuelve una tupla con una lista de reglas en forma de tuplas y 
+    una lista de paginas que contiene un listado de numeros cada una
+    '''
     reglas = []
     paginas =[]
     corte = False
@@ -35,6 +39,10 @@ def procesar_txt(txt):
     return reglas, paginas
 
 def procesar_reglas(reglas):
+    '''
+    Toma las reglas y crea un diccionario donde la clave es un numero de pagina
+    los valores es un listado de paginas que deben ir despues que la clave.
+    '''
     mapa_reglas = {}
     for regla in reglas:
         if regla[0] not in mapa_reglas.keys():
@@ -45,6 +53,10 @@ def procesar_reglas(reglas):
     return mapa_reglas
 
 def procesar_paginas(paginas, mapa_reglas):
+    '''
+    Chequea cuales paginas cumples con las condiciones del mapa_reglas
+    Devuelve un listado de booleanos
+    '''
     update_ok = []
     for update in paginas:
         orden = True
@@ -57,6 +69,9 @@ def procesar_paginas(paginas, mapa_reglas):
     return update_ok
 
 def suma_updates_correctos(updates, paginas):
+    '''
+    Imprime la suma de las paginas del medio del listado de updates que esten correctos
+    '''
     suma = []
     for i, orden in enumerate(updates):
         if orden:
@@ -64,6 +79,50 @@ def suma_updates_correctos(updates, paginas):
             suma.append(aux[len(aux)//2])
 
     print(sum(suma))
+    
+def ordenar_updates(paginas, updates, mapa_reglas):
+    '''
+    Para las paginas que no cumplen con el mapa de reglas, las reordena y devuelve un 
+    listado con las paginas reordenadas.
+    '''
+    paginas_reordenadas = []
+    for i, pagina in enumerate(paginas):
+        if not updates[i]:
+            for i, u in enumerate(pagina):
+                if procesar_paginas([pagina], mapa_reglas)[0]:
+                    continue
+                else:
+                    if u in mapa_reglas.keys():
+                        for j in range(0, i):
+                            if pagina[j] in mapa_reglas[u]:
+                                rotar_elementos(pagina, i, j)
+                    else:
+                        mover_al_final(pagina, i)
+            paginas_reordenadas.append(pagina)
+    return paginas_reordenadas
+
+def mover_al_final(lista, indice):
+    # Verifica que el índice sea válido
+    if 0 <= indice < len(lista):
+        elemento = lista.pop(indice)  # Extrae el elemento seleccionado
+        lista.append(elemento)       # Agrega el elemento al final
+    return lista
+
+def rotar_elementos(lista, i, j):
+    if 0 <= i < len(lista) - 1:  # Verifica que el índice sea válido
+        lista[i], lista[j] = lista[j], lista[i]
+    return lista
+
+def suma_updates_reordenados(paginas_reordenadas):
+    '''
+    Suma el elemento del medio de cada lista de paginas reordenadas
+    '''
+    suma = []
+    for p in paginas_reordenadas:
+        aux = p
+        suma.append(aux[len(aux)//2])
+
+    return sum(suma)
             
 #%%
 
@@ -100,10 +159,16 @@ reglas, paginas = procesar_txt(txt)
 mapa_reglas = procesar_reglas(reglas)
 updates = procesar_paginas(paginas, mapa_reglas)
 suma_updates_correctos(updates, paginas)
-        
+
+
+paginas_ordenadas = ordenar_updates(paginas, updates, mapa_reglas)
+print("La suma de las paginas reordenadas es: ", suma_updates_reordenados(paginas_ordenadas))
 #%% parte 1
 txt = leer_txt("input.txt")
 reglas, paginas = procesar_txt(txt)
 mapa_reglas = procesar_reglas(reglas)
 updates = procesar_paginas(paginas, mapa_reglas)
 suma_updates_correctos(updates, paginas)
+
+paginas_ordenadas = ordenar_updates(paginas, updates, mapa_reglas)
+print("La suma de las paginas reordenadas es: ", suma_updates_reordenados(paginas_ordenadas))
